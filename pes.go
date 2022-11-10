9a858662d9701aefa29147dda85347ea49a80a43
misodengaku <misodengaku@gmail.com>
2022-11-10 15:36:11 +0900

    refactor

--- pes.go
+++ pes.go
@@ -2,6 +2,7 @@ package mpeg2ts
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"sync"
 )
@@ -36,7 +37,17 @@ const (
 )
 
 var (
-	ErrAlreadyClosed = fmt.Errorf("PESParser is already closed")
+	ErrAlreadyClosed          = errors.New("PESParser is already closed")
+	ErrByteIncomingChanClosed = errors.New("byteIncomingChan is closed")
+	ErrCanceled               = errors.New("canceled")
+)
+
+const (
+	StateFindPrefix = iota
+	StateParseOptPESHeader
+	StateReadPacket
+	StateReadBytes
+	StateReadPaddingBytes
 )
 
 // Packetized Elementary Stream
@@ -105,146 +116,185 @@ func NewPESParser(ctx context.Context, bufferSize int) PESParser {
 	return pp
 }
 
+func (pp *PESParser) receiveBytes() ([]PESByte, error) {
+	var in []PESByte
+	select {
+	case <-pp.ctx.Done():
+		return nil, ErrCanceled
+	case w, ok := <-pp.byteIncomingChan:
+		if !ok {
+			return nil, ErrByteIncomingChanClosed
+		}
+		// enqueue bytes to parser queue
+		in = make([]PESByte, len(w))
+		copy(in, w)
+		return in, nil
+	}
+}
+
+func (pp *PESParser) findPrefix() bool {
+	if pp.getBufferLength() < 6 {
+		// not enough buffer
+		return false
+	}
+
+	// find prefix bytes
+	prefixIndex := -1
+	for i := 0; i < pp.getBufferLength()-3; i++ {
+		if pp.buffer[i].Datum == 0 && pp.buffer[i+1].Datum == 0 && pp.buffer[i+2].Datum == 1 {
+			// does not match prefix pattern. skip 1byte
+			prefixIndex = i
+			pp.dequeue(prefixIndex)
+			break
+		}
+	}
+	if prefixIndex == -1 {
+		// ran out of buffers. but no prefix pattern found
+		pp.dequeue(pp.getBufferLength())
+		return false
+	}
+
+	pp.PES.Prefix = uint32(pp.buffer[0].Datum)<<16 | uint32(pp.buffer[1].Datum)<<8 | uint32(pp.buffer[2].Datum)
+	pp.PES.StreamID = pp.buffer[3].Datum
+	pp.PES.PacketLength = uint16(pp.buffer[4].Datum)<<8 | uint16(pp.buffer[5].Datum)
+
+	pp.dequeue(6)
+	return true
+}
+
+func (pp *PESParser) readPaddingBytes() bool {
+	if pp.getBufferLength() > int(pp.PES.PacketLength) {
+		// not enough buffer
+		return false
+	}
+	pp.PES.Padding = make([]byte, pp.PES.PacketLength)
+	pp.mutex.Lock()
+	for i, v := range pp.buffer[:pp.PES.PacketLength] {
+		pp.PES.Padding[i] = v.Datum
+	}
+	pp.mutex.Unlock()
+	pp.dequeue(int(pp.PES.PacketLength))
+	return true
+}
+
+func (pp *PESParser) parseOptionalPESHeaders() {
+	pp.PES.ScramblingControl = (pp.buffer[0].Datum >> 4) & 0x03
+	pp.PES.Priority = (pp.buffer[0].Datum>>3)&0x01 == 1
+	pp.PES.DataAlignment = (pp.buffer[0].Datum>>2)&0x01 == 1
+	pp.PES.Copyright = (pp.buffer[0].Datum>>1)&0x01 == 1
+	pp.PES.Original = (pp.buffer[0].Datum)&0x01 == 1
+	pp.PES.PTSFlag = (pp.buffer[1].Datum>>7)&0x01 == 1
+	pp.PES.DTSFlag = (pp.buffer[1].Datum>>6)&0x01 == 1
+	pp.PES.ESCRFlag = (pp.buffer[1].Datum>>5)&0x01 == 1
+	pp.PES.ESRateFlag = (pp.buffer[1].Datum>>4)&0x01 == 1
+	pp.PES.DSMTrickModeFlag = (pp.buffer[1].Datum>>3)&0x01 == 1
+	pp.PES.AdditionalCopyInfoFlag = (pp.buffer[1].Datum>>2)&0x01 == 1
+	pp.PES.CRCFlag = (pp.buffer[1].Datum>>1)&0x01 == 1
+	pp.PES.ExtensionFlag = (pp.buffer[1].Datum)&0x01 == 1
+	pp.PES.HeaderDataLength = pp.buffer[2].Datum
+
+	// PES header
+	trimIndex := 2
+	if pp.PTSFlag {
+		// if (PTS_DTS_flags == '10') {
+		pp.PES.rawPTS = uint32((pp.buffer[3].Datum>>1)&0x07)<<30 | uint32(pp.buffer[4].Datum)<<22 | uint32(pp.buffer[5].Datum>>1)<<15 | uint32(pp.buffer[6].Datum)<<7 | uint32(pp.buffer[7].Datum>>1)
+		pp.PES.PTS = float64(pp.PES.rawPTS) / 90000 // 90kHz
+		trimIndex += 5
+	}
+	if pp.DTSFlag {
+		// if (PTS_DTS_flags == '11') {
+		pp.PES.rawDTS = uint32((pp.buffer[8].Datum>>1)&0x07)<<30 | uint32(pp.buffer[9].Datum)<<22 | uint32(pp.buffer[10].Datum>>1)<<15 | uint32(pp.buffer[11].Datum)<<7 | uint32(pp.buffer[12].Datum>>1)
+		pp.PES.DTS = float64(pp.PES.rawDTS) / 90000 // 90kHz
+		trimIndex += 5
+	}
+	if pp.ESCRFlag {
+		// pp.ESCRBase = pp.buffer
+	}
+	// ESRate
+	// DSMtrick
+	// additionalCopyInfo
+	// CRC
+	// PES extension
+
+	pp.dequeue(trimIndex + 1)
+}
+
 func (pp *PESParser) StartPESReadLoop() <-chan PES {
 	pc := make(chan PES, 16)
-	go func(pesOutChan chan PES) {
-		state := 0
+	go func(pesOutChan chan<- PES) {
+		state := StateFindPrefix
 		for {
-			var in []PESByte
-			select {
-			case <-pp.ctx.Done():
-				// fmt.Println("exit")
+			isLast := false
+
+			in, err := pp.receiveBytes()
+			if err != nil {
 				close(pesOutChan)
 				return
-			default:
-			case w, ok := <-pp.byteIncomingChan:
-				if !ok {
-					return
-				}
-				// enqueue bytes to parser queue
-				in = make([]PESByte, len(w))
-				copy(in, w)
 			}
 
-			isLast := false
 			for i := 0; i < len(in); i++ {
 				if in[i].EndOfStream {
 					in = in[:i]
 					isLast = true
+					break
 				}
 			}
 			pp.enqueue(in)
-		READLOOP:
+
+		ReadLoop:
 			for pp.getBufferLength() > 0 {
-				if state == 0 {
-					if pp.getBufferLength() < 6 {
-						// buffer is too short
-						break READLOOP
+				switch state {
+				case StateFindPrefix:
+					if ok := pp.findPrefix(); !ok {
+						break ReadLoop
 					}
-
-					// find prefix bytes
-					prefixIndex := -1
-					for i := 0; i < pp.getBufferLength()-3; i++ {
-						if pp.buffer[i].Datum == 0 && pp.buffer[i+1].Datum == 0 && pp.buffer[i+2].Datum == 1 {
-							prefixIndex = i
-							pp.dequeue(prefixIndex)
-							break
-						}
-					}
-					if prefixIndex == -1 {
-						// sync byte is not found
-						pp.dequeue(pp.getBufferLength())
-						break READLOOP
-					}
-
-					pp.PES.Prefix = uint32(pp.buffer[0].Datum)<<16 | uint32(pp.buffer[1].Datum)<<8 | uint32(pp.buffer[2].Datum)
-					pp.PES.StreamID = pp.buffer[3].Datum
-					pp.PES.PacketLength = uint16(pp.buffer[4].Datum)<<8 | uint16(pp.buffer[5].Datum)
-
-					pp.dequeue(6)
-					state = 1
-				}
-
-				if state == 1 {
+					state = StateParseOptPESHeader
+				case StateParseOptPESHeader:
 					if pp.getBufferLength() < 13 {
-						// buffer is too short
-						break READLOOP
+						// not enough buffer
+						break ReadLoop
 					}
 					switch pp.PES.StreamID {
 					default:
 						if (pp.buffer[0].Datum>>6)&0x03 != 0x02 {
-							// invalid. reset
+							// invalid marker bits. reset
 							pp.dequeue(1)
-							state = 0
-							break READLOOP
+							state = StateFindPrefix
+							break ReadLoop
 						}
 
-						pp.PES.ScramblingControl = (pp.buffer[0].Datum >> 4) & 0x03
-						pp.PES.Priority = (pp.buffer[0].Datum>>3)&0x01 == 1
-						pp.PES.DataAlignment = (pp.buffer[0].Datum>>2)&0x01 == 1
-						pp.PES.Copyright = (pp.buffer[0].Datum>>1)&0x01 == 1
-						pp.PES.Original = (pp.buffer[0].Datum)&0x01 == 1
-						pp.PES.PTSFlag = (pp.buffer[1].Datum>>7)&0x01 == 1
-						pp.PES.DTSFlag = (pp.buffer[1].Datum>>6)&0x01 == 1
-						pp.PES.ESCRFlag = (pp.buffer[1].Datum>>5)&0x01 == 1
-						pp.PES.ESRateFlag = (pp.buffer[1].Datum>>4)&0x01 == 1
-						pp.PES.DSMTrickModeFlag = (pp.buffer[1].Datum>>3)&0x01 == 1
-						pp.PES.AdditionalCopyInfoFlag = (pp.buffer[1].Datum>>2)&0x01 == 1
-						pp.PES.CRCFlag = (pp.buffer[1].Datum>>1)&0x01 == 1
-						pp.PES.ExtensionFlag = (pp.buffer[1].Datum)&0x01 == 1
-						pp.PES.HeaderDataLength = pp.buffer[2].Datum
-
-						// PES header
-						trimIndex := 2
-						if pp.PTSFlag {
-							// if (PTS_DTS_flags == '10') {
-							pp.PES.rawPTS = uint32((pp.buffer[3].Datum>>1)&0x07)<<30 | uint32(pp.buffer[4].Datum)<<22 | uint32(pp.buffer[5].Datum>>1)<<15 | uint32(pp.buffer[6].Datum)<<7 | uint32(pp.buffer[7].Datum>>1)
-							pp.PES.PTS = float64(pp.PES.rawPTS) / 90000 // 90kHz
-							trimIndex += 5
-						}
-						if pp.DTSFlag {
-							// if (PTS_DTS_flags == '11') {
-							pp.PES.rawDTS = uint32((pp.buffer[8].Datum>>1)&0x07)<<30 | uint32(pp.buffer[9].Datum)<<22 | uint32(pp.buffer[10].Datum>>1)<<15 | uint32(pp.buffer[11].Datum)<<7 | uint32(pp.buffer[12].Datum>>1)
-							pp.PES.DTS = float64(pp.PES.rawDTS) / 90000 // 90kHz
-							trimIndex += 5
-						}
-						if pp.ESCRFlag {
-							// pp.ESCRBase = pp.buffer
-						}
-						// ESRate
-						// DSMtrick
-						// additionalCopyInfo
-						// CRC
-						// PES extension
-
-						pp.dequeue(trimIndex + 1)
-						state = 2
+						pp.parseOptionalPESHeaders()
+						state = StateReadPacket
 
 					case StreamID_ProgramStreamMap:
-						fallthrough
+						// contains only PES_packet_data_byte
+						state = StateReadBytes
 					case StreamID_PrivateStream2:
-						fallthrough
+						// contains only PES_packet_data_byte
+						state = StateReadBytes
 					case StreamID_ECM:
-						fallthrough
+						// contains only PES_packet_data_byte
+						state = StateReadBytes
 					case StreamID_EMM:
-						fallthrough
+						// contains only PES_packet_data_byte
+						state = StateReadBytes
 					case StreamID_ProgramStreamDirectory:
-						fallthrough
+						// contains only PES_packet_data_byte
+						state = StateReadBytes
 					case StreamID_DSMCC:
-						fallthrough
+						// contains only PES_packet_data_byte
+						state = StateReadBytes
 					case StreamID_H222_1_TypeE:
-						state = 3
+						// contains only PES_packet_data_byte
+						state = StateReadBytes
 					case StreamID_PaddingStream:
-						state = 4
+						state = StateReadPaddingBytes
 					}
-
-				}
-
-				if state == 2 {
+				case StateReadPacket:
 					// read payload
 					if pp.getBufferLength() == 0 {
 						// buffer is empty
-						break READLOOP
+						break ReadLoop
 					}
 					writtenBytes := 0
 
@@ -254,7 +304,7 @@ func (pp *PESParser) StartPESReadLoop() <-chan PES {
 							pr := pp.PES.DeepCopy()
 							pesOutChan <- pr
 							pp.PES = PES{}
-							state = 0
+							state = StateFindPrefix
 							break
 						}
 						pp.PES.ElementaryStream = append(pp.PES.ElementaryStream, v.Datum)
@@ -262,12 +312,11 @@ func (pp *PESParser) StartPESReadLoop() <-chan PES {
 					}
 					pp.dequeue(writtenBytes)
 					pp.mutex.Unlock()
-				}
 
-				if state == 3 {
+				case StateReadBytes:
 					if pp.getBufferLength() < int(pp.PES.PacketLength) {
-						// buffer is too short
-						break READLOOP
+						// not enough buffer
+						break ReadLoop
 					}
 					pp.PES.PacketDataStream = make([]byte, pp.PES.PacketLength)
 
@@ -278,26 +327,15 @@ func (pp *PESParser) StartPESReadLoop() <-chan PES {
 					pp.mutex.Unlock()
 					fmt.Println(pp.PES.PacketLength)
 					pp.dequeue(int(pp.PES.PacketLength))
-					state = 0
-				}
+					state = StateFindPrefix
+				case StateReadPaddingBytes:
+					if ok := pp.readPaddingBytes(); ok {
+						state = StateFindPrefix
+					}
 
-				if state == 4 {
-					if pp.getBufferLength() > int(pp.PES.PacketLength) {
-						// buffer is too short
-						break READLOOP
-					}
-					pp.PES.Padding = make([]byte, pp.PES.PacketLength)
-					pp.mutex.Lock()
-					for i, v := range pp.buffer[:pp.PES.PacketLength] {
-						pp.PES.Padding[i] = v.Datum
-					}
-					pp.mutex.Unlock()
-					pp.dequeue(int(pp.PES.PacketLength))
-					state = 0
 				}
 			}
 			if isLast {
-				// fmt.Println("last packet received")
 				close(pesOutChan)
 				return
 			}
